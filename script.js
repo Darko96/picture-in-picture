637501a9096d7efa82059b2b2eb11c6b1e4d38e8
unknown <darkomijatovic2022@gmail.com>
2023-02-10 15:10:14 +0100

    added some comments in js file

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ async function selectMediaStream() {
   try {
     // Kreiramo konstantu koja ce imati podatke u vidu objekta o klipu koji smo izabrali
     // i koji zelimo da pustimo, dakle cekamo dok korisnik ne odabre klip
+    // Ova funkcija u sustini zahteva od korisnika da odabere video izvor
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     // Onda postavljamo da nas html element (videoElement) sadrzi taj objekat kao src tj link
     // za prikazivanje koji dobija od mediaStream konstante
+    // U sustini dodeljuje izabrani izvor videa elementu
     videoElement.srcObject = mediaStream;
     // Onda kada je taj video ucitaj, tj treba da znamo da ce
     // onloadedmetadata biti true kada se zavrsi ucitavanje i onda ce se pozvati arrow funckija
     // koja ce da pusti taj video
     videoElement.onloadedmetadata = () => {
+      // Kada se video ucita, ova funkcija pokreće video
       videoElement.play();
     };
   } catch (error) {
@@ -22,6 +25,9 @@ async function selectMediaStream() {
   }
 }
 
+// Dugme ima event listener koji poziva funkciju "videoElement.requestPictureInPicture"
+// kada se klikne, što omogućava funkciju Picture in Picture.
+// Nakon što se funkcija izvrši, dugme se ponovo omogućava.
 button.addEventListener("click", async () => {
   // Disable Button
   // Postavljamo da dugme nije aktivno i da na dugme nije moguce kliknuti
@@ -34,7 +40,9 @@ button.addEventListener("click", async () => {
   // Reset Button
   // Ponovo aktiviramo dugme
   button.disabled = false;
+  console.log("daa");
 });
 
 // On Load
+// Funkcija "selectMediaStream" se poziva automatski prilikom učitavanja stranice.
 selectMediaStream();
